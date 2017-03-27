5d7b83e1091f31b5ed2c02811565fc831b66ed32
Fanny Breuneval <fbreuneval@ensc.fr>
2017-03-27 12:04:30 +0200

    pasfini_tdemployes

---
+++ POO2/employes/Employe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace employes
+{
+    class Employe
+    {
+        protected string _nom;
+        protected int _matricule;
+        protected double _indSalarial;
+        private static double _salaireBase=1480;
+        private static int _compteur=0;
+
+        private Employe (string nom, double ind)
+    {
+        _nom=nom;
+        _indSalarial=ind;
+        _matricule = _compteur;
+        _compteur++;
+    }
+        public int Matricule // !! pas de parenthèses pour les méthodes get/set
+        {get{ return _matricule;}}
+
+        public double SalaireBase 
+        {
+            get ;
+            set ;
+        }
+
+        public double Salaire
+        {
+            get { return _indSalarial*_salaireBase;}
+           set { _indSalarial=value;}
+        }
+
+
+        }
+    }
---
+++ POO2/employes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace employes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employe jj = new Employe("J.J", 1.2);
+            jj.Matricule();
+
+            Console.ReadKey();  
+        }
+    }
+}
